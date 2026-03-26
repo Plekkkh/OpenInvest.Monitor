7de14010141d78a3fd5cda6a2a184c7eeb97fc2e
JeekaMIX <genyaplehanov2006@ya.ru>
2026-03-26 18:56:48 +0500

    add logging configuration for T-Invest service

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,39 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'tinkoff_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR / 'tinkoff.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'portfolio.services.t_invest': {
+            'handlers': ['console', 'tinkoff_file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
